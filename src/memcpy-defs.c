bec7c529024a9fa44046e8fd1b2efadf33ece326
Noah Goldstein <goldstein.w.n@gmail.com>
2021-08-06 18:16:01 -0400

    avx2 versions added (not tuned)

--- src/memcpy-defs.c
+++ src/memcpy-defs.c
@@ -9,7 +9,12 @@ void * memcpy_dev_v32_movsb_v20(void *, const void *, size_t);
 void * memcpy_dev_v32_movsb_v21(void *, const void *, size_t);
 void * memcpy_dev_v32_movsb_v22(void *, const void *, size_t);
 void * memcpy_dev_v32_movsb_avx2(void *, const void *, size_t);
-
+void * memcpy_glibc_v32_movsb_avx2(void *, const void *, size_t);
+void * memcpy_dev_v32_movsb_avx2(void *, const void *, size_t);
+void * memcpy_dev_v32_movsb_avx2_v19(void *, const void *, size_t);
+void * memcpy_dev_v32_movsb_avx2_v20(void *, const void *, size_t);
+void * memcpy_dev_v32_movsb_avx2_v21(void *, const void *, size_t);
+void * memcpy_dev_v32_movsb_avx2_v22(void *, const void *, size_t);
 
 #define NAME memcpy_glibc_v32_movsb
 #include <bench/bench-memcpy.h>
@@ -34,7 +39,27 @@ void * memcpy_dev_v32_movsb_avx2(void *, const void *, size_t);
 #include <bench/bench-memcpy.h>
 #undef NAME
 
+
+#define NAME memcpy_glibc_v32_movsb_avx2
+#include <bench/bench-memcpy.h>
+#undef NAME
+
+
 #define NAME memcpy_dev_v32_movsb_avx2
+#include <bench/bench-memcpy.h>
+#undef NAME
+
+#define NAME memcpy_dev_v32_movsb_avx2_v19
+#include <bench/bench-memcpy.h>
+#undef NAME
+
+#define NAME memcpy_dev_v32_movsb_avx2_v20
+#include <bench/bench-memcpy.h>
+#undef NAME
+#define NAME memcpy_dev_v32_movsb_avx2_v21
+#include <bench/bench-memcpy.h>
+#undef NAME
+#define NAME memcpy_dev_v32_movsb_avx2_v22
 #include <bench/bench-memcpy.h>
 #undef NAME
 
@@ -49,6 +74,11 @@ const memcpy_info_t memcpy_defs[] = {
     make_memcpy_info(memcpy_dev_v32_movsb_v20),
     make_memcpy_info(memcpy_dev_v32_movsb_v21),
     make_memcpy_info(memcpy_dev_v32_movsb_v22),
-    make_memcpy_info(memcpy_dev_v32_movsb_avx2)
+    make_memcpy_info(memcpy_glibc_v32_movsb_avx2),
+    make_memcpy_info(memcpy_dev_v32_movsb_avx2),
+    make_memcpy_info(memcpy_dev_v32_movsb_avx2_v19),
+    make_memcpy_info(memcpy_dev_v32_movsb_avx2_v20),
+    make_memcpy_info(memcpy_dev_v32_movsb_avx2_v21),
+    make_memcpy_info(memcpy_dev_v32_movsb_avx2_v22)
 };
 const uint64_t nmemcpy_defs = sizeof(memcpy_defs) / sizeof(memcpy_defs[0]);
