edc1726b303d81450754834088cfea4ba887ee6a
Noah Goldstein <goldstein.w.n@gmail.com>
2021-08-10 13:33:30 -0400

    run7

--- quick-parse.py
+++ quick-parse.py
@@ -11,8 +11,8 @@ key_order = []
 results = {}
 
 
-def make_key(a, b, c):
-    return str(a) + "-" + str(b) + "-" + str(c)
+def make_key(a, b, c, d):
+    return str(a) + "-" + str(b) + "-" + str(c) + "-" + str(d)
 
 
 def process(d):
@@ -24,29 +24,32 @@ with open(fname) as csvfile:
     reader = csv.DictReader(csvfile, fieldnames=header)
     for line in reader:
         impl = line["impl name"].replace("memcpy_", "").replace(
-            "_v32_movsb", "").replace("_avx2", "").lstrip().rstrip()
+            "_v32_movsb", "").lstrip().rstrip()
 
         size = line["size"].lstrip().rstrip()
         min_sz = line["al dst"].lstrip().rstrip()
         max_sz = line["al src"].lstrip().rstrip()
+        nconfs = line["dst > src"].lstrip().rstrip()
         t = float(line["geomean"].lstrip().rstrip()) / float(line["dst > src"])
-        k = make_key(size, min_sz, max_sz)
+        k = make_key(size, min_sz, max_sz, nconfs)
         if k not in results:
             results[k] = {}
             key_order.append(k)
 
         if impl not in results[k]:
-            results[k][impl] = [size, min_sz, max_sz, [t]]
+            results[k][impl] = [size, min_sz, max_sz, nconfs, [t]]
         else:
-            results[k][impl][3].append(t)
+            results[k][impl][4].append(t)
 
-print("{}, {}, {}, {}, {}, {}".format("size scale", "min size", "max size",
-                                      "new time", "cur time", "new / cur"))
+print("{}, {}, {}, {}, {}, {}, {}".format("size scale", "min size", "max size",
+                                          "nconfs", "new time", "cur time",
+                                          "new / cur"))
 for k in key_order:
     res = results[k][v1]
-    td = process(results[k][v1][3])
-    tg = process(results[k][v2][3])
+    td = process(results[k][v1][4])
+    tg = process(results[k][v2][4])
 
     improve = round(100.0 * float(td / tg), 2)
-    print("{}, {}, {}, {}, {}, {}".format(res[0], res[1], res[2], round(td, 3),
-                                          round(tg, 3), improve))
+    print("{}, {}, {}, {}, {}, {}, {}".format(res[0], res[1], res[2], res[3],
+                                              round(td, 3), round(tg, 3),
+                                              improve))
