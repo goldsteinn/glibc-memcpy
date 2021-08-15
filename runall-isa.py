c6d1281a1d3a395ea3b6b56ae0174e009a1c2c04
Noah Goldstein <goldstein.w.n@gmail.com>
2021-08-15 03:39:25 -0400

    more config

--- runall-isa.py
+++ runall-isa.py
@@ -151,8 +151,10 @@ for isa in isa_list:
         for scale in scales:
             for direction in directions:
                 confs.append(Config(func, isa, 0, -1, scale, direction, 8192))
+                confs.append(Config(func, isa, 0, -1, scale, direction, 65536))
                 if do_test_mode:
                     break
+                continue
                 for maximum in no_min_maxes:
                     confs.append(
                         Config(func, isa, 0, maximum, scale, direction, 8192))
